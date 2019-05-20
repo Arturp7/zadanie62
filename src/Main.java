b69708a403ce27ffa9ddb2ffa4920a9abfa64cf4
Artur Przyjemski <spinezpl@gmail.com>
2019-05-20 13:03:33 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] names = new String[3];
+
+        names[0] = "Aleksandra";
+        names[1] = "Żaneta";
+        names[2] = "Martyna";
+
+        System.out.println("Podaje trzy imiona żeńskie:  " + names[0] +" "+  names[1]+ " i "+ names[2]);
+
+
+    }
+}
